fdb00cf44df0a76ae4cda9bac683552ac6462812
AnweshPattnaik2006 <anshcrp@gmail.com>
2026-01-29 16:10:38 +0530

    Add simple calculator program
    Added comments to calculator program

--- calculator.py
+++ calculator.py
@@ -1,1 +1,12 @@
+# Simple Calculator Program
+# Author: Anwesh
+# Description: Performs basic arithmetic operations
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Sum =", a + b)
+print("Difference =", a - b)
+print("Product =", a * b)
+
 
